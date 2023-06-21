f93f86b5df9e9fe2adc75ed33ec732ae41423d33
H252003 <mohamedhesham252003@gmail.com>
2023-06-21 16:47:28 +0300

    lcd displays what you write with wrapping

--- lcd_first/lcd_first.ino
+++ lcd_first/lcd_first.ino
@@ -2,25 +2,27 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
 
+String st = "BatMan Begins";
 void setup()
 {
+  Serial.begin(9600);
   lcd.init(); // initialize the lcd
   lcd.backlight();
-}
+} 
 
 void loop()
 {
-  lcd.clear();                 // clear display
-  lcd.setCursor(0, 0);         // move cursor to   (0, 0)
-  lcd.print("Arduino");        // print message at (0, 0)
-  lcd.setCursor(2, 1);         // move cursor to   (2, 1)
-  lcd.print("GetStarted.com"); // print message at (2, 1)
-  delay(2000);                 // display the above for two seconds
+  if(Serial.available() != 0)
+  st = Serial.readString();
+  st.trim();
+  lcd.clear();
+  lcd.setCursor(0,0);
 
-  lcd.clear();                  // clear display
-  lcd.setCursor(3, 0);          // move cursor to   (3, 0)
-  lcd.print("DIYables");        // print message at (3, 0)
-  lcd.setCursor(0, 1);          // move cursor to   (0, 1)
-  lcd.print("www.diyables.io"); // print message at (0, 1)
-  delay(2000);                  // display the above for two seconds
+    String st1 = st;
+    lcd.print(st1);
+    st1.remove(0,16);
+    lcd.setCursor(0,1);
+    lcd.print(st1);    
+    
+  delay(2000);
 }
